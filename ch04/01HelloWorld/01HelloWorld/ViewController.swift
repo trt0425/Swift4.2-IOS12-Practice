839bb1e390acea6eeebbd7eff656f5f314196a55
Hank <hank@2001-b400-e4af-b052-913b-0555-1503-5cfb.emome-ip6.hinet.net>
2019-02-17 16:43:05 +0800

    20190217-04-01完成

--- ch04/01HelloWorld/01HelloWorld/ViewController.swift
+++ ch04/01HelloWorld/01HelloWorld/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func onClick(_ sender: Any) {
+        label.text = "Hello World!!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
