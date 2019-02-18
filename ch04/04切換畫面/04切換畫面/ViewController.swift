7288b5c67e546e2e81f69d7f43c1e1b024202234
Hank <hank@Seande-MacBook-Pro.local>
2019-02-18 11:42:09 +0800

    Finish04-04

---
+++ ch04/04切換畫面/04切換畫面/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04切換畫面
+//
+//  Created by Hank on 2019/2/18.
+//  Copyright © 2019 Hank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
