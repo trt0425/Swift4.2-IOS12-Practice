e1eb2435b095f5d7a07e461ebe518d22c8da06cf
Hank <hank@Seande-MacBook-Pro.local>
2019-02-19 14:16:13 +0800

    0504

---
+++ ch05/04用程式建立Constraint/04用程式建立Constraint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04用程式建立Constraint
+//
+//  Created by Hank on 2019/2/19.
+//  Copyright © 2019 Hank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
